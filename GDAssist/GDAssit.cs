be514ba10547390d1558bed6211bae961341d762
potokki <pokki.hokki@gmail.com>
2017-07-10 17:30:00 -0500

    Add Commands, GDAssit, and GDAssitTest

---
+++ GDAssist/GDAssit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GDAssist
+{
+    public class GDAssit
+    {
+
+        public void SSS()
+        {
+            int i = 3;
+            i += 3;
+        }
+    }
+}
---
+++ Test/GDAssistTest/UT.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GDAssistTest
+{
+    [TestClass]
+    public class UT
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
